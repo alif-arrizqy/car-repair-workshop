68d64b579737fa57c5198089d27f65275ccc4ec6
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-07-23 15:26:16 +0700

    migration supabase

--- prisma/migrations/20240723082306_init/migration.sql
+++ prisma/migrations/20240723082306_init/migration.sql
@@ -1,9 +1,9 @@
 -- CreateTable
-CREATE TABLE "roles" (
+CREATE TABLE "role" (
     "id" SERIAL NOT NULL,
     "name" TEXT NOT NULL,
 
-    CONSTRAINT "roles_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "role_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateTable
@@ -12,7 +12,7 @@ CREATE TABLE "user" (
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
     "password" TEXT NOT NULL,
-    "role" INTEGER NOT NULL,
+    "role_id" INTEGER NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
 
@@ -30,22 +30,11 @@ CREATE TABLE "jenis_kerusakan" (
 );
 
 -- CreateTable
-CREATE TABLE "gambar_service" (
-    "id" SERIAL NOT NULL,
-    "id_service" INTEGER NOT NULL,
-    "foto_before" TEXT NOT NULL,
-    "foto_after" TEXT NOT NULL,
-    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updated_at" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "gambar_service_pkey" PRIMARY KEY ("id")
-);
-
--- CreateTable
 CREATE TABLE "service" (
     "id" SERIAL NOT NULL,
+    "id_service" TEXT NOT NULL,
     "id_user" INTEGER NOT NULL,
-    "acceped" BOOLEAN NOT NULL,
+    "accepted" BOOLEAN NOT NULL,
     "status_pekerjaan" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -54,22 +43,21 @@ CREATE TABLE "service" (
 );
 
 -- CreateTable
-CREATE TABLE "service_jenis_kerusakan" (
+CREATE TABLE "service_detail" (
     "id" SERIAL NOT NULL,
-    "id_service" INTEGER NOT NULL,
+    "id_service" TEXT NOT NULL,
     "id_jenis_kerusakan" INTEGER NOT NULL,
+    "foto_before" TEXT NOT NULL,
+    "foto_after" TEXT NOT NULL,
 
-    CONSTRAINT "service_jenis_kerusakan_pkey" PRIMARY KEY ("id")
+    CONSTRAINT "service_detail_pkey" PRIMARY KEY ("id")
 );
 
 -- AddForeignKey
-ALTER TABLE "user" ADD CONSTRAINT "user_role_fkey" FOREIGN KEY ("role") REFERENCES "roles"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "user" ADD CONSTRAINT "user_role_id_fkey" FOREIGN KEY ("role_id") REFERENCES "role"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "service" ADD CONSTRAINT "service_id_user_fkey" FOREIGN KEY ("id_user") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE "service_jenis_kerusakan" ADD CONSTRAINT "service_jenis_kerusakan_id_service_fkey" FOREIGN KEY ("id_service") REFERENCES "service"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
-
--- AddForeignKey
-ALTER TABLE "service_jenis_kerusakan" ADD CONSTRAINT "service_jenis_kerusakan_id_jenis_kerusakan_fkey" FOREIGN KEY ("id_jenis_kerusakan") REFERENCES "jenis_kerusakan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE "service_detail" ADD CONSTRAINT "service_detail_id_jenis_kerusakan_fkey" FOREIGN KEY ("id_jenis_kerusakan") REFERENCES "jenis_kerusakan"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
