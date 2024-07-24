13f5ffb1afde723d629d7bb417b1bd029e6b2ace
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-07-24 14:34:54 +0700

    feat: Add user creation functionality to UsersController

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -1,8 +1,30 @@
 import type { Request, Response } from "express";
+import { createUserSchema } from "../helpers/validation/user.validation";
+import { ResponseHelper } from "../helpers/response/response.helper";
+import { createUserService } from "../services/user.service";
 
 class UsersController {
   // create a new user
-  createUser = async (req: Request, res: Response) => {}
+  createUser = async (req: Request, res: Response) => {
+    // create user schema validation
+    const parsed = createUserSchema.safeParse(req.body);
+    if (parsed.success) {
+      // create user service
+      const user = await createUserService(parsed.data);
+      if (user.status) {
+        console.log(`Create user success: ${user.message}`);
+        res.json(ResponseHelper.successMessage(user.message, 201));
+      } else {
+        console.log(`Create user failed: ${user.message}`);
+        res.json(ResponseHelper.errorMessage(user.message, 400));
+      }
+    } else {
+      console.log(
+        `Create user failed, User data is invalid: ${parsed.error.errors}`
+      );
+      res.json(ResponseHelper.errorData(parsed.error.errors, 400));
+    }
+  };
 
   // get all users
   getAllUsers = async (req: Request, res: Response) => {};
