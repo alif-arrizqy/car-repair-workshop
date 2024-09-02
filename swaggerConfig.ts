e32f2a98a53810bb944c96517eb35fda38f3d71c
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-09-02 11:50:42 +0700

    feat: Add schema response and update User to Swagger configuration

--- swaggerConfig.ts
+++ swaggerConfig.ts
@@ -33,6 +33,156 @@ const options = {
             },
           },
         },
+        UpdateUser: {
+          type: "object",
+          properties: {
+            name: {
+              type: "string",
+              example: "John Doe",
+            },
+            email: {
+              type: "string",
+              example: "john.doe@email.com",
+            },
+            role_id: {
+              type: "number",
+              example: 1,
+            },
+          },
+        },
+        ErrorData: {
+          type: "object",
+          properties: {
+            message: {
+              type: "object",
+              properties: {
+                statusCode: {
+                  type: "number",
+                  example: 400,
+                },
+                status: {
+                  type: "string",
+                  example: "failed",
+                },
+                data: {
+                  type: "object",
+                  example: {
+                    message: "Error message",
+                  },
+                },
+              },
+            },
+          },
+        },
+        ErrorMessage: {
+          type: "object",
+          properties: {
+            message: {
+              type: "object",
+              properties: {
+                statusCode: {
+                  type: "number",
+                  example: 400,
+                },
+                status: {
+                  type: "string",
+                  example: "failed",
+                },
+                message: {
+                  type: "string",
+                  example: "Error message",
+                },
+              },
+            },
+          },
+        },
+        SuccessDataArray: {
+          type: "object",
+          properties: {
+            message: {
+              type: "object",
+              properties: {
+                statusCode: {
+                  type: "number",
+                  example: 200,
+                },
+                status: {
+                  type: "string",
+                  example: "success",
+                },
+                data: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    properties: {
+                      name: {
+                        type: "string",
+                        example: "John Doe",
+                      },
+                      email: {
+                        type: "string",
+                        example: "john.doe@email.com",
+                      },
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        SuccessDataObject: {
+          type: "object",
+          properties: {
+            message: {
+              type: "object",
+              properties: {
+                statusCode: {
+                  type: "number",
+                  example: 200,
+                },
+                status: {
+                  type: "string",
+                  example: "success",
+                },
+                data: {
+                  type: "object",
+                  properties: {
+                    name: {
+                      type: "string",
+                      example: "John Doe",
+                    },
+                    email: {
+                      type: "string",
+                      example: "john.doe@email.com",
+                    },
+                  },
+                },
+              },
+            },
+          },
+        },
+        SuccessMessage: {
+          type: "object",
+          properties: {
+            message: {
+              type: "object",
+              properties: {
+                statusCode: {
+                  type: "number",
+                  example: 200,
+                },
+                status: {
+                  type: "string",
+                  example: "success",
+                },
+                message: {
+                  type: "string",
+                  example: "Success message",
+                },
+              },
+            },
+          },
+        },
       },
     },
   },
