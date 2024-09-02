937c52cab34a6dd998b973fac416c159fe71c0f4
alif-arrizqy <alif.arrizqy20@gmail.com>
2024-09-02 16:26:16 +0700

    feat: create new password reset and change password schemas for swagger

---
+++ swaggers/auth.swagger.ts
@@ -0,0 +1,45 @@
+const AuthScehams = {
+  RequestResetPassword: {
+    type: "object",
+    properties: {
+      email: {
+        type: "string",
+        format: "email",
+        example: "john.doe@email.com",
+      },
+    },
+  },
+  ResetPassword: {
+    type: "object",
+    properties: {
+      token: {
+        type: "string",
+        example: "secret-token",
+      },
+      email: {
+        type: "string",
+        format: "email",
+        example: "john.doe@email.com",
+      },
+    },
+  },
+  ChangePassword: {
+    type: "object",
+    properties: {
+      id: {
+        type: "string",
+        example: "user-id",
+      },
+      oldPassword: {
+        type: "string",
+        example: "old-password",
+      },
+      newPassword: {
+        type: "string",
+        example: "new-password",
+      },
+    },
+  },
+};
+
+export default AuthScehams;
